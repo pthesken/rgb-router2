0e62541f2d27ffa4d27999fb9ddba4aac31d7cfa
Patrick Thesken <patrick@student.dev-island.org>
2024-03-05 19:16:59 -1000

    set up red green and blue routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
+function Red() {
+  return <div className="page red">Red</div>;
+}
+
+function Green() {
+  return <div className="page green">Green</div>;
+}
+
+function Blue() {
+  return <div className="page blue">Blue</div>;
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Red />} />
+        <Route path="/green" element={<Green />} />
+        <Route path="/blue" element={<Blue />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
